295d0d6d9f60a8ff711bad2fc14f953f42d333c6
PEGutierrF <pgutierr@uvm.edu>
2023-10-27 17:38:35 -0400

    data

--- Temporal Stability Analysis.R
+++ Temporal Stability Analysis.R
@@ -18,11 +18,17 @@ rm(list = ls())
 
 
 data <- read_excel('raw_data/ts_data.xlsx', sheet = 'carapa')
-head(data,20)
+head(data,6)
 
 
 # Transform Species Richness Carapa
 shapiro.test(data$Carapa_Sp_Rich)
 Carapa_Sp_Rich_new <- sqrt(data$Carapa_Sp_Rich+1)
 shapiro.test(Carapa_Sp_Rich_new)
+
+# Transform Productivity Carapa
+shapiro.test(data$Carapa_Biomass_TS)
+Carapa_Biomass_TS_new <- 1/(data$Carapa_Biomass_TS)
+shapiro.test(Carapa_Biomass_TS_new)
+
 
