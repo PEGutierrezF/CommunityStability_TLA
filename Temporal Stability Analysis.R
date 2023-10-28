d4a080a31f620dfd4a0460a357fcd240d3a92377
PEGutierrF <pgutierr@uvm.edu>
2023-10-27 23:07:53 -0400

    data

--- Temporal Stability Analysis.R
+++ Temporal Stability Analysis.R
@@ -18,7 +18,7 @@ rm(list = ls())
 
 
 data <- read_excel('raw_data/ts_data.xlsx', sheet = 'ts')
-head(data,6)
+head(data.frm,6)
 
 
 
@@ -81,22 +81,53 @@ cD
 # Saltito -----------------------------------------------------------------
 
 
-# Transform Species Richness Carapa
+# Transform Species Richness Saltito
 # Square root transformation
 shapiro.test(data$Saltito_Sp_Rich)
 Saltito_Sp_Rich_new <- sqrt(data$Saltito_Sp_Rich+2)
 shapiro.test(Saltito_Sp_Rich_new)
 
-# Transform Productivity Carapa
+# Transform Productivity Saltito
 # Reciprocal (inverse) transformation
 shapiro.test(data$Saltito_Biomass_TS)
 Saltito_Biomass_TS_new <- 1/sqrt(data$Saltito_Biomass_TS)
 shapiro.test(Saltito_Biomass_TS_new)
 
-# Transform Density Carapa
-# Reciprocal (inverse) transformation
-shapiro.test(as.numeric(na.omit(data$Saltito_Density_TS)))
-Saltito_Density_TS_new <- as.numeric(na.omit(1/sqrt(data$Saltito_Density_TS)))
-shapiro.test(Carapa_Density_TS_new)
+
+
+data_new <- data %>% 
+  select(Saltito_Sp_Rich, Saltito_Density_TS)%>%
+  filter(row_number() != 118)
+# Convert Saltito_Density_TS to numeric
+data_new$Saltito_Density_TS <- as.numeric(data_new$Saltito_Density_TS)
+
+# Transform Species Richness Saltito
+# Square root transformation
+shapiro.test(data_new$Saltito_Sp_Rich)
+Saltito_Sp_Rich_na_omit <- sqrt(data_new$Saltito_Sp_Rich+2)
+shapiro.test(Saltito_Sp_Rich_na_omit)
+
+
+# Check the Shapiro-Wilk test
+shapiro.test(data_new$Saltito_Density_TS)
+Saltito_Density_TS_na_omit <- 1/sqrt(data_new$Saltito_Density_TS)
+shapiro.test(Saltito_Density_TS_na_omit)
+
+
+
+
+# Linnear regression
+mod3 <- lm(Saltito_Biomass_TS_new ~ Saltito_Sp_Rich_new)
+# Print the summary of the regression model
+summary(mod3)
+
+
+# Linnear regression
+mod4 <- lm(Saltito_Density_TS_na_omit ~ Saltito_Sp_Rich_na_omit)
+# Print the summary of the regression model
+summary(mod4)
+
+
+
 
 
